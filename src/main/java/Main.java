969bf59ad0e6925ab2430a89d30bff268d41e1a8
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-17 16:47:50 +0530

    Removing dead code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -55,24 +55,7 @@ public class Main implements IOProvider
 
 
     }
-
-
-    public static void mainx(String[] args)
-    {
-        List<Document> documents = new ArrayList<>();
-        documents.add(new Document("the total number of documents by the sexy newton of documents containing the term, and then taking the logarithm of that quotient", 1));
-        documents.add(new Document("A formula that aims to define the importance of a keyword or phrase within a document or a web page", 2));
-        documents.add(new Document("to the more meaningful terms brown newton cow ", 3));
-        documents.add(new Document("ccurs in each document; the number of times a term in  keyword a document is called its term frequency.", 4));
-
-        TextSearchIndex index = SearchIndexFactory.buildIndex(documents);
-
-        String searchTerm = "total number of documents";
-
-        var batch = index.search(searchTerm);
-        batch.forEach(item -> System.out.println(item.getUniqueIdentifier() + " -- " + item.getRelevanceScore()));
-
-    }
+    
 
     @Override
     public String readLine()
